13d694650bde4fd6e50395bff741f28e1b9a37e0
Patch Neranartkomol <patch@neranartkomol.com>
2020-11-16 09:29:01 -0800

    Privacy Policy Stub

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -4,13 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const PrivacyPolicy = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <SEO title="Privacy Policy" />
+    <h1>Privacy Policy</h1>
+    <p>Welcome to privacy Policy</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default PrivacyPolicy
